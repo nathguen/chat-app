a3fbd85ed160bea0c0942f4bced6bc4158ab2d26
Nathan <nathan.guenther.web@gmail.com>
2023-04-05 21:00:36 -0600

    feat(chat): add auto-scroll feature fix(chat): prevent navbar collapse with block text

--- public/js/chat.js
+++ public/js/chat.js
@@ -31,6 +31,29 @@ const getUserData = () => {
   return userData;
 };
 
+const autoScroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - (newMessageHeight * 2) <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -89,6 +112,9 @@ function displayMessage(message, template) {
   if (username === "Admin" && text.includes("has left")) {
     leaveAudio.play();
   }
+
+  // auto scroll
+  autoScroll();
 }
 
 socket.on("locationMessage", (message) => {
